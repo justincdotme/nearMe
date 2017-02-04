6ddd6886ebafbd93084dd6f3b6f842af86608ed1
Justin Christenson <info@justinc.me>
2017-02-04 15:33:53 -0800

    NM-1 Added Geocoding fx

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$app->post('address',[
+    'as' => 'pages.home',
+    'uses' => 'LocationController@address'
+]);
 
-$app->get('/', function () use ($app) {
-    return $app->version();
-});
+$app->post('coords/{lat}/{lng}',[
+    'as' => 'pages.home',
+    'uses' => 'LocationController@coords'
+]);
