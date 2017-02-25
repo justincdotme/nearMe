f9834186243dd7df86c54d31918f92db4636b9d4
Justin Christenson <info@justinc.me>
2017-02-25 11:16:00 -0800

    NM-2 Format fix

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015']
-            }
+                }
             },
             {
                 test: /\.scss$/,
